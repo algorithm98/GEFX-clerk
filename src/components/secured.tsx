79374409489695aa67ac240ab17e398c16739fed
Prince Prakalp <30505797+algorithm98@users.noreply.github.com>
2024-10-03 17:49:44 +0530

    Update secured.tsx
    Security Update

--- src/components/secured.tsx
+++ src/components/secured.tsx
@@ -4,7 +4,7 @@ export default function Secured() {
   return (
     <div>
       <div className='flex text-2xl font-bold mt-20 items-center justify-center'>
-        <p>The Only Dashboard, Secured With Clerk-THCL Secutiry V4</p>
+        <p>The Only Dashboard, Secured With V4</p>
       </div>
     </div>
   )
